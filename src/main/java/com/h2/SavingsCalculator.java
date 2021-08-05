b311086a48dce320bbd870b43ae91b147ae029bb
Johnston, Peter <peter.johnston@wizards.com>
2021-08-05 10:29:10 -0700

    Completed module

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,16 @@
+package com.h2;
+
+public class SavingsCalculator {
+
+    private float[] credits;
+    private float[] debits;
+
+    public float SavingsCalculator(float[] credits, float[] debits) {
+
+        private float sumOfCredits(){
+            return 0.0f;
+        }
+    }
+}
+
+
